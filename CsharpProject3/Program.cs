2f72da4467eddadf2f311ad9d27d7c017718c4d1
ZharifZubaidi <zharif.aizuddin@stratusauto.com>
2025-07-05 17:38:04 +0800

    add: part 2 ongoing

--- CsharpProject3/Program.cs
+++ CsharpProject3/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 // Decision logic: if-else exercise
+
 /****************************
  * Exercise 1
  ***************************/
+/*
 Random dice = new Random();
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -39,10 +42,12 @@ else
 {
     Console.WriteLine("You lose!");
 }
+*/
 
 /****************************
  * Exercise 2
  ***************************/
+/*
 string message = "The quick brown fox jumps over the lazy dog";
 
 bool result = message.Contains("dog");  // Check for substring existence in a string and return boolean result
@@ -50,6 +55,45 @@ Console.WriteLine(result);
 
 if (message.Contains("fox"))
 {
-    Console.WriteLine("What does the fox say?");
+   Console.WriteLine("What does the fox say?");
 }
+*/
 
+/****************************
+ * Exercise 3
+ ***************************/
+
+Random random = new();
+int daysUntilExpire = random.Next(16);  // 2 weeks. passed argument is a exclusion
+int discountPercentage = 0;
+
+daysUntilExpire = 5;    // test
+
+Console.WriteLine($"{daysUntilExpire} days");
+
+if (daysUntilExpire <= 14)
+{
+    if (daysUntilExpire >= 6 && daysUntilExpire <= 14)
+    {
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+    else if (daysUntilExpire > 1 && daysUntilExpire <= 5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysUntilExpire} days.\nRenew now and save {discountPercentage}%");
+    }
+    else if (daysUntilExpire == 1)
+    {
+        discountPercentage = 20;
+        Console.WriteLine($"Your subscription expires within a day. \nRenew now and save {discountPercentage}%");
+    }
+    else
+    {
+        Console.WriteLine("Your subscription has expired.");
+    }
+}
+else
+{
+    Console.WriteLine("Please continue watching!");
+}
+    
