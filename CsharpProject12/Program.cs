27e8d8552d67c371abdf7ceefc8745d16698b0e3
ZharifZubaidi <zharif.aizuddin@stratusauto.com>
2025-07-09 19:49:04 +0800

    add: part 4 ongoing

---
+++ CsharpProject12/Program.cs
@@ -0,0 +1,111 @@
+﻿
+// Data types
+
+string? userInput = ""; // declare the string to expect a null value
+Console.Write("Please select your example: ");
+userInput = Console.ReadLine();
+
+if (userInput != null)
+{
+    if (userInput == "1")
+    {
+        // Signed integral type
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 1:");
+        Console.WriteLine("*****************************");
+
+        Console.WriteLine("Signed integral types:");
+
+        Console.WriteLine($"sbyte\t: {sbyte.MinValue} to {sbyte.MaxValue}");
+        Console.WriteLine($"short\t: {short.MinValue} to {short.MaxValue}");
+        Console.WriteLine($"int\t: {int.MinValue} to {int.MaxValue}");
+        Console.WriteLine($"long\t: {long.MinValue} to {long.MaxValue}");
+
+    }
+    else if (userInput == "2")
+    {
+        // Unsigned integral type
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 2:");
+        Console.WriteLine("*****************************");
+
+        Console.WriteLine("");
+        Console.WriteLine("Unsigned integral types:");
+
+        Console.WriteLine($"byte\t: {byte.MinValue} to {byte.MaxValue}");
+        Console.WriteLine($"ushort\t: {ushort.MinValue} to {ushort.MaxValue}");
+        Console.WriteLine($"uint\t: {uint.MinValue} to {uint.MaxValue}");
+        Console.WriteLine($"ulong\t: {ulong.MinValue} to {ulong.MaxValue}");
+
+    }
+    else if (userInput == "3")
+    {
+        // Floating-point types
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 3:");
+        Console.WriteLine("*****************************");
+
+        Console.WriteLine("");
+        Console.WriteLine("Floating point types:");
+        Console.WriteLine($"float : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+        Console.WriteLine($"double: {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+        Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+    }
+    else if (userInput == "4")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 4:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "5")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 5:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "6")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 6:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "7")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 7:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "8")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExercise 8:");
+        Console.WriteLine("*****************************");
+
+    }
+    else if (userInput == "9")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExercise 2:");
+        Console.WriteLine("*****************************");
+
+    }
+
+    Console.WriteLine("Thank you! Please press enter to exit.");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Please try again");
+}
+
---
+++ CsharpProject13/Program.cs
@@ -0,0 +1,184 @@
+﻿
+// Casting and conversion techniques
+
+string? userInput = ""; // declare the string to expect a null value
+Console.Write("Please select your example: ");
+userInput = Console.ReadLine();
+
+if (userInput != null)
+{
+    if (userInput == "1")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 1:");
+        Console.WriteLine("*****************************");
+
+        // int first = 2;
+        // string second = "4";
+        // int result = first + second;
+        // Console.WriteLine(result);
+    }
+    else if (userInput == "2")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 2:");
+        Console.WriteLine("*****************************");
+
+        int myInt = 3;
+        Console.WriteLine($"int: {myInt}");
+
+        decimal myDecimal = myInt;
+        Console.WriteLine($"decimal: {myDecimal}");
+    }
+    else if (userInput == "3")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 3:");
+        Console.WriteLine("*****************************");
+
+        decimal myDecimal = 3.14m;
+        Console.WriteLine($"decimal: {myDecimal}");
+
+        int myInt = (int)myDecimal;
+        Console.WriteLine($"int: {myInt}");
+
+    }
+    else if (userInput == "4")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 4:");
+        Console.WriteLine("*****************************");
+        decimal myDecimal = 1.23456789m;
+        float myFloat = (float)myDecimal;
+
+        Console.WriteLine($"Decimal: {myDecimal}");
+        Console.WriteLine($"Float  : {myFloat}");
+
+    }
+    else if (userInput == "5")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 5:");
+        Console.WriteLine("*****************************");
+        int first = 5;
+        int second = 7;
+        string message = first.ToString() + second.ToString();
+        Console.WriteLine(message);
+
+    }
+    else if (userInput == "6")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 6:");
+        Console.WriteLine("*****************************");
+        string first = "5";
+        string second = "7";
+        int sum = int.Parse(first) + int.Parse(second);
+        Console.WriteLine(sum);
+
+    }
+    else if (userInput == "7")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 7:");
+        Console.WriteLine("*****************************");
+        string value1 = "5";
+        string value2 = "7";
+        int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+        Console.WriteLine(result);
+
+    }
+    else if (userInput == "8")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 8:");
+        Console.WriteLine("*****************************");
+        int value = (int)1.5m; // casting truncates
+        Console.WriteLine(value);
+
+        int value2 = Convert.ToInt32(1.5m); // converting rounds up
+        Console.WriteLine(value2);
+
+    }
+    else if (userInput == "9")
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 9:");
+        Console.WriteLine("*****************************");
+        string value = "102";
+        int result = 0;
+        if (int.TryParse(value, out result))
+        {
+            Console.WriteLine($"Measurement: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Unable to report the measurement.");
+        }
+        Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+    }
+    else if (userInput == "10")
+    {
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 10:");
+        Console.WriteLine("*****************************");
+        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+        decimal total = 0m;
+        string message = "";
+
+        foreach (var value in values)
+        {
+            decimal number; // stores the TryParse "out" value
+            if (decimal.TryParse(value, out number))
+            {
+                total += number;
+            }
+            else
+            {
+                message += value;
+            }
+        }
+
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Total: {total}");
+    }
+    else if (userInput == "11")
+    {
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 11:");
+        Console.WriteLine("*****************************");
+
+        int value1 = 11;
+        decimal value2 = 6.2m;
+        float value3 = 4.3f;
+
+        int result1 = Convert.ToInt32(value1 / value2);
+        Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+        decimal result2 = value2 / (decimal)value3;
+        Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+        float result3 = value3 / value1;
+        Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+    }
+
+    Console.WriteLine("Thank you! Please press enter to exit.");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Please try again");
+}
+
