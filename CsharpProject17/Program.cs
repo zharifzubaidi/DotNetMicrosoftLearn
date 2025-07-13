3df26dbe0fd3cfcd7bae15008b7a37c5cf6fb3d2
zharifzubaidi <zharif96@gmail.com>
2025-07-13 09:48:48 +0800

    add: part 5 ongoing

---
+++ CsharpProject17/Program.cs
@@ -0,0 +1,41 @@
+﻿// Class methods
+
+string? userInput = ""; // declare the string to expect a null value
+Console.Write("Please select your example: ");
+userInput = Console.ReadLine();
+
+switch(userInput)
+{
+    case "1":
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 1:");
+        Console.WriteLine("*****************************");
+        break;
+    }
+    case "2":
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 2:");
+        Console.WriteLine("*****************************");
+        break;
+    }
+    case "3":
+    {
+        // 
+        Console.WriteLine("*****************************");
+        Console.WriteLine("\tExample 3:");
+        Console.WriteLine("*****************************");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Wrong input. Please try again");
+        break;
+    }
+}
+   
+Console.WriteLine("Thank you! Please press enter to exit.");
+Console.ReadLine();
