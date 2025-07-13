f8e02b675d18d3bc693b7a1ea0e9986071a2cd3d
zharifzubaidi <zharif96@gmail.com>
2025-07-13 23:10:56 +0800

    add: part 6 ongoing

--- CsharpProject18/Program.cs
+++ CsharpProject18/Program.cs
@@ -9,6 +9,9 @@ This code uses a names array and corresponding methods to display
 greeting messages
 */
 
+using System.Diagnostics;
+using System.Linq.Expressions;
+
 string? userInput = ""; // declare the string to expect a null value
 Console.Write("Please select your example: ");
 userInput = Console.ReadLine();
@@ -352,9 +355,344 @@ switch (userInput)
         }
     case "8":
         {
-            // Example
+            // Throw exception
             Console.WriteLine("*****************************");
             Console.WriteLine("\tExample 8:");
+            Console.WriteLine("*****************************");
+            string[][] userEnteredValues = new string[][]
+            {
+                    new string[] { "1", "two", "3"},
+                    new string[] { "0", "1", "2"}
+            };
+
+            foreach (string[] userEntries in userEnteredValues)
+            {
+                try
+                {
+                    BusinessProcess1(userEntries);
+                }
+                catch (Exception ex)
+                {
+                    if (ex.StackTrace.Contains("BusinessProcess1") && (ex is FormatException))
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            static void BusinessProcess1(string[] userEntries)
+            {
+                int valueEntered;
+
+                foreach (string userValue in userEntries)
+                {
+                    try
+                    {
+                        valueEntered = int.Parse(userValue);
+
+                        // completes required calculations based on userValue
+                        // ...
+                    }
+                    catch (FormatException)
+                    {
+                        FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+                        throw invalidFormatException;
+                    }
+                }
+            }
+
+            break;
+        }
+    case "9":
+        {
+            // Exception
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 9:");
+            Console.WriteLine("*****************************");
+
+            try
+            {
+                OperatingProcedure1();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exiting application.");
+            }
+
+            static void OperatingProcedure1()
+            {
+                string[][] userEnteredValues = new string[][]
+                {
+                    new string[] { "1", "two", "3"},
+                    new string[] { "0", "1", "2"}
+                };
+
+                foreach(string[] userEntries in userEnteredValues)
+                {
+                    try
+                    {
+                        BusinessProcess1(userEntries);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.StackTrace.Contains("BusinessProcess1"))
+                        {
+                            if (ex is FormatException)
+                            {
+                                Console.WriteLine(ex.Message);
+                                Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                            }
+                            else if (ex is DivideByZeroException)
+                            {
+                                Console.WriteLine(ex.Message);
+                                Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                                // re-throw the original exception
+                                throw;
+                            }
+                            else
+                            {
+                                // create a new exception object that wraps the original exception
+                                throw new ApplicationException("An error occurred - ", ex);
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            static void BusinessProcess1(string[] userEntries)
+            {
+                int valueEntered;
+
+                foreach (string userValue in userEntries)
+                {
+                    try
+                    {
+                        valueEntered = int.Parse(userValue);
+
+                        checked
+                        {
+                            int calculatedValue = 4 / valueEntered;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+                        throw invalidFormatException;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+                        throw unexpectedDivideByZeroException;
+
+                    }
+                }
+            }
+            break;
+        }
+    case "10":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 10:");
+            Console.WriteLine("*****************************");
+
+            // Prompt the user for the lower and upper bounds
+            Console.Write("Enter the lower bound: ");
+            int lowerBound = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the upper bound: ");
+            int upperBound = int.Parse(Console.ReadLine());
+
+            decimal averageValue = 0;
+
+            bool exit = false;
+
+            do
+            {
+                try
+                {
+                    // Calculate the sum of the even numbers between the bounds
+                    averageValue = AverageOfEvenNumbers(lowerBound, upperBound);
+
+                    // Display the value returned by AverageOfEvenNumbers in the console
+                    Console.WriteLine($"The average of even numbers between {lowerBound} and {upperBound} is {averageValue}.");
+
+                    exit = true;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("An error has occured.");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"The upper bound must be greater than {lowerBound}");
+                    Console.WriteLine($"Enter a new upper bound: ");
+                    string? userResponse = Console.ReadLine();
+                    if (userResponse.ToLower().Contains("exit"))
+                    {
+                        exit = true;
+                    }
+                    else
+                    {
+                        exit = false;
+                        upperBound = int.Parse(userResponse);
+                    }
+                }
+            } while (exit == false);
+
+
+            // Wait for user input
+            Console.ReadLine();
+
+            static decimal AverageOfEvenNumbers(int lowerBound, int upperBound)
+            {
+                if (lowerBound >= upperBound)
+                {
+                    throw new ArgumentOutOfRangeException("upperBound", "ArgumentOutOfRangeException: upper bound must be greater than lower bound.");
+                }
+            
+                int sum = 0;
+                int count = 0;
+                decimal average = 0;
+
+                for (int i = lowerBound; i <= upperBound; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                        count++;
+                    }
+                }
+
+                average = (decimal)sum / count;
+
+                return average;
+            }
+
+            break;
+        }
+    case "11":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 11:");
+            Console.WriteLine("*****************************");
+
+            string[][] userEnteredValues = new string[][]
+            {
+                        new string[] { "1", "2", "3"},
+                        new string[] { "1", "two", "3"},
+                        new string[] { "0", "1", "2"}
+            };
+
+            try
+            {
+                Workflow1(userEnteredValues);
+                Console.WriteLine("'Workflow1' completed successfully.");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("An error occurred during 'Workflow1'.");
+                Console.WriteLine(ex.Message);
+            }
+
+            static void Workflow1(string[][] userEnteredValues)
+            {
+
+                foreach (string[] userEntries in userEnteredValues)
+                {
+                    try
+                    {
+                        Process1(userEntries);
+                        Console.WriteLine("'Process1' completed successfully.");
+                        Console.WriteLine();
+                    }
+                    catch(FormatException ex)
+                    {
+                        Console.WriteLine("'Process1' encountered an issue, process aborted.");
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            static void Process1(String[] userEntries)
+            {
+                int valueEntered;
+
+                foreach (string userValue in userEntries)
+                {
+                    bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+                    if (integerFormat == true)
+                    {
+                        if (valueEntered != 0)
+                        {
+                            checked
+                            {
+                                int calculatedValue = 4 / valueEntered;
+                            }
+                        }
+                        else
+                        {
+                            throw new DivideByZeroException("Invalid data. User input value must be  non-zero values");
+                        }
+                    }
+                    else
+                    {
+                        throw new FormatException("Invalid data. User input values must be valid integers.");
+                    }
+                }
+            }
+
+            break;
+        }
+    case "12":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 12:");
+            Console.WriteLine("*****************************");
+            break;
+        }
+    case "13":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 13:");
+            Console.WriteLine("*****************************");
+            break;
+        }
+    case "14":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 14:");
+            Console.WriteLine("*****************************");
+            break;
+        }
+    case "15":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 15:");
+            Console.WriteLine("*****************************");
+            break;
+        }
+    case "16":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 16:");
+            Console.WriteLine("*****************************");
+            break;
+        }
+    case "17":
+        {
+            // Example
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\tExample 17:");
             Console.WriteLine("*****************************");
             break;
         }
